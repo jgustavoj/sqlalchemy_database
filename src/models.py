1bea88da0402aeba6720901339412119979e9be6
jgustavoj <gjime011@fiu.edu>
2020-10-29 01:26:20 +0000

    completed

--- src/models.py
+++ src/models.py
@@ -11,7 +11,6 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'User'
     id = Column(Integer, primary_key=True)
-<<<<<<< HEAD
     username = Column(String(250), nullable=False, unique=True)
     firstname = Column(String(250), nullable=False, unique=False)
     lastname = Column(String(250), nullable=False, unique=False)
@@ -41,38 +40,6 @@ class Comment(Base):
     comment_text = Column(String(250), nullable=False, unique=False)
     author_id = Column(Integer, ForeignKey('User.id'), nullable=False, unique=False)
     post_id = Column(Integer, ForeignKey('Post.id'), nullable=False, unique=False)
-=======
-    username = Column(String(250), nullable=False)
-    firstname = Column(String(250), nullable=False)
-    lastname = Column(String(250), nullable=False)
-    email = Column(String(250), nullable=False)
-
-    #primary key? foreignkey? for email
-
-class Comment(Base):
-    __tablename__ = 'Comment'
-    id = Column(Integer, primary_key=True)
-    comment_text = Column(String(250))
-    author_id = Column(Integer, ForeignKey('User.id'))
-    post_id = Column(Integer, ForeignKey('Post.id'))
-
-class Post(Base):
-    __tablename__ = 'Post'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('User.id'))
-
-# class Media(Base):
-#     __tablename__= 'Media'
-#     id = Column(Integer, primary_key=(True))
-#     media_type = Column(enumerate(250), nullable=False )
-#     url = Column(String(250), nullale = True )
-#     post_id = (Integer, ForeignKey('Post.id'))
-
-# class Follower(Base):
-#     __tablename__='Follower'
-#     user_from_id = Column(Integer, nullable=False )
-#     user_to_id = Column(Integer, nullable=False )
->>>>>>> ee6a873896d9b022b2790955ba7cf6dc2b8456f3
 
 
     def to_dict(self):
